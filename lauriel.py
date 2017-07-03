bd1336bb0506a5984fb83c5399c12b5dd158ae57
Mateusz Druzkowski <mdruzko@ra.rockwell.com>
2017-07-03 20:29:06 +0200

    lauriel.py expanded, loop with expection and csv file (just a test)

--- lauriel.py
+++ lauriel.py
@@ -1,9 +1,23 @@
 #! /usr/bin/env python
 
+import csv
 from setup_crawler import gather_webaddr, validate_webaddr
 
 
 def lauriel():
     starting_website = raw_input("Input url of a website you wish to 'scan': ")
-    raw_websites = gather_webaddr(starting_website)
-    validated_websites = validate_webaddr(raw_websites)
+    while True:
+        try:
+            raw_websites = gather_webaddr(starting_website)
+            validated_websites = validate_webaddr(raw_websites)
+            with open("websites.csv", "wb") as csvfile:
+               f = csv.writer(csvfile, delimeter=',') 
+               f.writerow(validated_websites)
+        except (KeyboardInterrupt, SystemExit):
+            raise
+        except:
+            print("KeyboardInterrupt has bit raised")
+
+
+if __name__ == "__main__":
+    lauriel()
