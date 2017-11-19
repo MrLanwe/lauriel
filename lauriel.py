43ec2aeb0fc82c2251ee619b57907f9119498c3c
Lanwe <m.druzkowski@gmail.com>
2017-11-19 23:11:48 +0100

    added checking if csv file exists and creating in if it doesn't. added posibility to input multiple url addresses to scan instead of one. validated addresses after initial scan are added to the list of websites to scan. added function docstring and todo comment for next step

--- lauriel.py
+++ lauriel.py
@@ -1,23 +1,53 @@
 #! /usr/bin/env python
 
+import os
 import csv
 from setup_crawler import gather_webaddr, validate_webaddr
 
 
 def lauriel():
-    starting_website = input("Input url of a website you wish to 'scan': ")
+
+    """ Input startin url or urls and lauriel will 'scan' all of the provided
+    addresses and all addresses that were found during 'scanning'"""
+
+    # TODO write a handler in validate_webaddr for website's that no longer
+    # exist or are otherwise unreachable
+
+    filename = 'websites.csv'
+    urls_from_single_website = []
+    starting_websites = input("Input url of a websites you wish to 'scan'"
+                              "seperated by space: ")
+    list_of_websites = starting_websites.split()
+
+    if os.path.exists(filename):
+        open_mod = 'a+'
+    else:
+        f = open(filename, 'w')
+        f.close
+        open_mod = 'a+'
+
     while True:
+
+        print(list_of_websites)
+        if 'validated_websites' in locals():
+            list_of_websites = urls_from_single_website
+            del urls_from_single_website[:]
+
         try:
-            raw_websites = gather_webaddr(starting_website)
-            validated_websites = validate_webaddr(raw_websites)
-            with open("websites.csv", "w") as csvfile:
-                f = csv.writer(csvfile, delimiter=',')
-                f.writerow(validated_websites)
-            print(raw_websites)
+            for url in list_of_websites:
+                raw_websites = gather_webaddr(url)
+                validated_websites = validate_webaddr(raw_websites)
+                with open(filename, open_mod) as csvfile:
+                    f = csv.writer(csvfile, delimiter=',')
+                    f.writerow(validated_websites)
+
+                urls_from_single_website += validate_webaddr
+                print(url)
         except (KeyboardInterrupt, SystemExit):
-            print("KeyboardInterrupt has bit raised")
-            break
+                print("KeyboardInterrupt has bit raised")
+                break
 
 
 if __name__ == "__main__":
     lauriel()
+
